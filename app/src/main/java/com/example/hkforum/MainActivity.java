6418661b6191e19da2ddeb69f6a5f1a67d4e3aad
Matthew_aR <longpingtai@gmail.com>
2021-05-11 21:56:38 +0800

    Try first time

---
+++ app/src/main/java/com/example/hkforum/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.hkforum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
